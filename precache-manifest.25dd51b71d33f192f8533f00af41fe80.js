81fd2f830b2a4aa90df9921c295ec6785e1e987c
Seibeaaar <yuribar4520@gmail.com>
2020-04-16 22:25:04 +0300

    Updates

---
+++ precache-manifest.25dd51b71d33f192f8533f00af41fe80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6627ea5f96f2eb317aad4423d4fa264f",
+    "url": "/Task-Manager/index.html"
+  },
+  {
+    "revision": "87036569d28f44508464",
+    "url": "/Task-Manager/static/css/main.dbc912a7.chunk.css"
+  },
+  {
+    "revision": "8a63dc30f1d6ea961236",
+    "url": "/Task-Manager/static/js/2.3e1f2645.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Task-Manager/static/js/2.3e1f2645.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87036569d28f44508464",
+    "url": "/Task-Manager/static/js/main.4965c5d9.chunk.js"
+  },
+  {
+    "revision": "8f61a6d22d0a1ad5acd5",
+    "url": "/Task-Manager/static/js/runtime-main.b9e78fc9.js"
+  },
+  {
+    "revision": "7c4dbc10aa3ea38539e0cd315064f485",
+    "url": "/Task-Manager/static/media/authorize-bg.7c4dbc10.png"
+  }
+]);
